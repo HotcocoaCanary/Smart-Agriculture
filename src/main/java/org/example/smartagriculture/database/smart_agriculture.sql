1f317809e995f4b8ff15d69fabe2c4f9af2f0e76
Canary <hotcocoa.canary@outlook.com>
2024-07-11 11:28:14 +0800

    7-11 数据库文件更新

--- src/main/java/org/example/smartagriculture/database/smart_agriculture.sql
+++ src/main/java/org/example/smartagriculture/database/smart_agriculture.sql
@@ -4,22 +4,19 @@ use smart_agriculture;
 
 create table smart_user
 (
-    Id       int auto_increment primary key,
-    Username varchar(30) null,
-    Password varchar(30) null,
-    Nickname varchar(30) null,
-    Email    varchar(30) null,
-    Phone    varchar(30) null
+    id       int auto_increment primary key,
+    username varchar(30) null,
+    password varchar(30) null,
+    nickname varchar(30) null,
+    email    varchar(30) null,
+    phone    varchar(30) null
 );
 
 create table smart_device
 (
-    Id          int auto_increment,
-    Device_name varchar(30) null,
+    id          int auto_increment primary key,
+    device_name varchar(30) null,
     create_at   time        null,
-    UserId      int         null,
-    constraint smart_device_pk
-        primary key (Id),
-    constraint smart_device_smart_user_Id_fk
-        foreign key (UserId) references smart_user (Id)
+    master      int         null,
+    constraint smart_device_smart_user_Id_fk foreign key (master) references smart_user (id)
 );
