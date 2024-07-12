b3889c6c34b63b793433980fdef8d1ebe6566d7c
Canary <hotcocoa.canary@outlook.com>
2024-07-12 10:27:31 +0800

    7-12 新增新闻表

--- src/main/java/org/example/smartagriculture/database/smart_agriculture.sql
+++ src/main/java/org/example/smartagriculture/database/smart_agriculture.sql
@@ -20,3 +20,18 @@ create table smart_device
     master      int         null,
     constraint smart_device_smart_user_Id_fk foreign key (master) references smart_user (id)
 );
+create table smart_news
+(
+    id             int auto_increment
+        primary key,
+    title          varchar(100) null,
+    content        text         null,
+    file_url       varchar(255) null,
+    creat_user_id  int          null,
+    update_user_id int          null,
+    delete_flag    tinyint      null,
+    constraint smart_news_smart_user_id_fk
+        foreign key (creat_user_id) references smart_user (id),
+    constraint smart_news_smart_user_id_fk_2
+        foreign key (update_user_id) references smart_user (id)
+);
